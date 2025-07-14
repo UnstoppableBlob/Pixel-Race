9ac78387071da0fa43a136ff5d502be49a11a140
UnstoppableBlob <96858668+UnstoppableBlob@users.noreply.github.com>
2025-07-14 12:20:16 -0700

    cleaned up code

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,7 +6,6 @@ const SPRINT_SPEED = 8.0
 const JUMP_VELOCITY = 6
 const SENSITIVITY = 0.004
 
-#bob variables
 const BOB_FREQ = 2.4
 const BOB_AMP = 0.08
 var t_bob = 0.0
@@ -15,7 +14,6 @@ var t_bob = 0.0
 const BASE_FOV = 75.0
 const FOV_CHANGE = 1.5
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 15
 
 @onready var head = $Head
@@ -47,21 +45,17 @@ func _physics_process(delta):
 		elif picked_object != null:
 			remove_object()
 	
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
 	if Input.is_action_pressed("spacebar") and is_on_floor():
 		jump()
 	
-	# Handle Sprint.
 	if Input.is_action_pressed("sprint"):
 		speed = SPRINT_SPEED
 	else:
 		speed = WALK_SPEED
 
-	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("a", "d", "w", "s")
 	var direction = (head.transform.basis * transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if is_on_floor():
@@ -75,11 +69,9 @@ func _physics_process(delta):
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
 	
-	# Head bob
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = _headbob(t_bob)
 	
-	# FOV
 	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
 	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
 	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
