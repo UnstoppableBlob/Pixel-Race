666544aa57595d3bc4806b0394fe8b81fbb659a4
UnstoppableBlob <96858668+UnstoppableBlob@users.noreply.github.com>
2025-07-23 21:18:28 -0700

    Gave up on the idea where the block tries to hit the player, and started working on the level design for level 2.
    Also added a smaller spike (gotten from www.kenney.nl)

--- scripts/spike_big.gd
+++ scripts/spike_big.gd
@@ -3,18 +3,19 @@ extends Node3D
 
 @export var fling_strength: float = 100.0
 
-@export var target_node: CharacterBody3D
+#@export var target_node: CharacterBody3D
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is RigidBody3D:
 		var rigid_body = body as RigidBody3D
 		
-		if target_node:
-			var direct_target = (target_node.global_position - rigid_body.global_position).normalized()
-			rigid_body.apply_central_impulse(direct_target * fling_strength)
-			print("worked")
-		else:
-			print("target_node isn't assigned")
-	else:
-		print("entered but no rigidbody")
+		var random_direction = Vector3(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0), randf_range(-1.0, 1.0)).normalized()
+		
+		rigid_body.apply_central_impulse(random_direction * fling_strength)
+		#if target_node:
+			#var direct_target = (target_node.global_position - rigid_body.global_position).normalized()
+			#rigid_body.apply_central_impulse(direct_target * fling_strength)
+		
+		# ^ this code is just what I had earlier. I am trying to figure out how to make the player take knockback from this, but I'll figure that out later
+		
