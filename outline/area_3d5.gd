18f963ca4b330613e39641d942efdf350690ce45
UnstoppableBlob <96858668+UnstoppableBlob@users.noreply.github.com>
2025-07-26 15:15:39 -0700

    Added the right and left winds.
    Had to fix a little error which made the particle process not work properly.

---
+++ outline/area_3d5.gd
@@ -0,0 +1,22 @@
+extends Area3D
+
+var rigid = null
+var yes
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is RigidBody3D:
+		rigid = body
+		#body.apply_central_impulse(Vector3(0, 10, 0))
+		print("works")
+		yes = true
+
+
+func _on_body_exited(body: Node3D) -> void:
+	if body is RigidBody3D:
+		yes = false
+
+
+func _process(delta: float) -> void:
+	if yes:
+		if rigid:
+			rigid.apply_central_impulse(Vector3(0, 0, -0.05))
