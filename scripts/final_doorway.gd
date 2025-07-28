3b8d35d0443685b3618a7087ffd4e2420a12a3f4
UnstoppableBlob <96858668+UnstoppableBlob@users.noreply.github.com>
2025-07-28 13:55:21 -0700

    Added the hexagon tile. Made the final doorway, and finished the game.

---
+++ scripts/final_doorway.gd
@@ -0,0 +1,37 @@
+extends Node3D
+
+@onready var anim = $"door-rotate2/AnimationPlayer"
+
+var open = false
+var is_used = false
+var exit = false
+
+signal update_buttons()
+
+func _on_distance_checker_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		if !open:
+			anim.play("open")
+			open = true
+
+
+func _on_distance_checker_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		if open:
+			anim.play("close")
+			open = false
+
+
+func _on_inside_checker_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		body.remove_object()
+		Transition.transition("final_doorway", global_position.x,global_position.y,global_position.z)
+		#await Transition.on_transition_finished
+		#if !is_used:
+			#global.level += 1
+			#update_buttons.emit()
+			#print(global.level)
+		#is_used = true
+		
+func _on_reset():
+	is_used = false
